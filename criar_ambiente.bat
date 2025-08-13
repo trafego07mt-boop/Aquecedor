ad4081596d3a03408191003901e38f3d55a07ef9
trafego07mt-boop <trafego07mt@gmail.com>
2025-08-13 14:37:35 -0300

    added logging messages to criar_ambiente

--- criar_ambiente.bat
+++ criar_ambiente.bat
@@ -1,7 +1,9 @@
 @echo off
 
 cls
+echo Configurando Ambiente...
 python -m venv .venv
 call .venv\Scripts\activate
+echo Verificando dependencias...
 python -m pip install -U -q pip
 pip install -U -q -r requirements.txt
